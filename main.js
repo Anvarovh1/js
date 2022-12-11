719e0dec7aa3dcfb3397999e53a5ce6390ab0fbf
anvarov_h <anvarovhumoyunmirzo1@gmail.com>
2022-12-11 19:55:29 +0500

    Java Script

---
+++ main.js
@@ -0,0 +1,79 @@
+let time = prompt('kerakli vaqtni kiriting')
+switch (time) {
+    case '0':
+        console.log('soat kechgi 12 boldi')
+        break;
+
+        case '1':
+        console.log('soat kechgi 1 boldi')
+        break;
+        case '2':
+        console.log('soat kechgi 2 boldi')
+        break;
+        case '3':
+        console.log('soat kechgi 3 boldi')
+        break;
+        case '4':
+        console.log('soat kechgi 4 boldi')
+        break;
+        case '5':
+        console.log('soat ertalabgi 5 boldi')
+        break;
+        case '6':
+        console.log('soat ertalabgi 6 boldi')
+        break;
+        case '7':
+        console.log('soat ertalabgi 7 boldi')
+        break;
+        case '8':
+        console.log('soat ertalabgi 8 boldi')
+        break;
+        case '9':
+        console.log('soat ertalabgi 9 boldi')
+        break;
+        case '10':
+        console.log('soat ertalabgi 10 boldi')
+        break;
+        case '11':
+        console.log('soat ertalabgi 11 boldi')
+        break;
+        case '12':
+        console.log('soat ertalabgi 12 boldi')
+        break;
+        case '13':
+        console.log('soat kunduzgi 1 boldi')
+        break;
+        case '14':
+        console.log('soat kunduzgi 2 boldi')
+        break;
+        case '15':
+        console.log('soat kunduzgi 3 boldi')
+        break;
+        case '16':
+        console.log('soat kechgi 4 boldi')
+        break;
+        case '17':
+        console.log('soat kechgi 5 boldi')
+        break;
+        case '18':
+        console.log('soat kechgi 6 boldi')
+        break;
+        case '19':
+        console.log('soat kechgi 7 boldi')
+        break;
+        case '20':
+        console.log('soat kechgi 8 boldi')
+        break;
+        case '21':
+        console.log('soat kechgi 9 boldi')
+        break;
+        case '22':
+        console.log('soat kechgi 10 boldi')
+        break;
+        case '23':
+        console.log('soat kechgi 11 boldi')
+        break;
+    default:
+    console.log("vaqt notog`ri ko`rsatildi")
+        break;
+}
